6507196e8370500b3183c2b1b3b40edd7f440744
Modhura002 <modhura.banerjee@gmail.com>
2025-07-22 18:21:11 +0530

    new addition

--- app.js
+++ app.js
@@ -20,10 +20,12 @@
      image.classList.remove("hide");
      cond.classList.remove("hide");
      wind.classList.remove("hide");
+     humid.classList.remove("hide");
     info.innerText=data.current.temp_c+"\u00B0C";
     image.src="https:"+data.current.condition.icon;
     cond.innerText=data.current.condition.text;
     wind.innerText="wind speed : "+data.current.wind_kph+"kph"+"wind dir : "+data.current.wind_dir;
+    humid.innerText="humidity : "+data.current.humidity;
 
     console.log(data);}
     catch(error){
@@ -31,6 +33,7 @@
          image.classList.add("hide");
          cond.classList.add("hide");
          wind.classList.add("hide");
+          humid.classList.add("hide");
         console.log(error);
         info.style.fontSize="30px";
         info.innerText="sorry couldnot get the data";
